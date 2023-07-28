609444a812fdfa8300729e468f48f4f49536d3f9
Sanghoon Lee <nasi3611@naver.com>
2023-07-29 01:31:27 +0900

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RSP_Game());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RSP_Game extends StatelessWidget {
+  const RSP_Game({super.key});
 
   // This widget is the root of your application.
   @override
